1f5125e041af4180fa2780ae6c0798fe710f2576
UnconsciousChess <hacker_snowwolf@qq.com>
2024-06-15 19:17:32 +0800

    修正少量bug，增加具体函数报错提示，修改针对前端传入数据处理的方法

--- Source/Generator.py
+++ Source/Generator.py
@@ -84,7 +84,7 @@ def FolderCreator(case,OutputDir,TemplateListDir) -> None:
                             continue
                         # 检查该行对应的文件是否存在，不存在则报错并跳过
                         if not os.path.exists(Filedir):
-                            print("ReadTemplateList函数报错：文件不存在或路径错误")
+                            print("ReadTemplateList函数报错：%s文件不存在或路径错误" % Filedir.split("\\")[-1])
                             continue
                         # 将文件路径和FileType组成元组，加入到字典中
                         TemplateFilesDict[CurrentTemplateType].append((Filedir,int(FileType)))
