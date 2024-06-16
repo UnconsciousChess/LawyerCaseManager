e3c066809b00ae194cc2c76c70d994b8b418aacc
UnconsciousChess <hacker_snowwolf@qq.com>
2024-06-16 19:48:42 +0800

    分离案件信息表单和当事人信息表单，增强代码模块化

---
+++ interface/src/components/LitigantForm.vue
@@ -0,0 +1,30 @@
+<template>
+	<el-form>
+		<el-form-item label="原告名字">
+			<el-input v-model="litigantForm.plaintiffName" />
+		</el-form-item>
+		<el-form-item label="原告身份证号">
+			<el-input v-model="litigantForm.plaintiffIdNumber" />
+		</el-form-item>
+		<el-form-item label="原告电话号码">
+			<el-input v-model="litigantForm.plaintiffPhoneNumber" />
+		</el-form-item>
+	</el-form>
+</template>
+
+
+<script setup>
+
+import { ref, onMounted , reactive} from 'vue'
+
+
+const litigantForm = ref({
+	plaintiffName: "",
+	plaintiffIdNumber: "",
+	plaintiffPhoneNumber: "",
+});
+
+
+const litigantFormRef = ref(null);
+
+</script>
