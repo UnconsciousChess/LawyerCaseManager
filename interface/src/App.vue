c2a00f559d38f40c51bacaeffeb45dce57fffff1
UnconsciousChess <hacker_snowwolf@qq.com>
2024-06-20 22:50:44 +0800

    小修改

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,13 +1,16 @@
-
-<!-- 组合式Vue -->
 <script setup>
 import AppMenu from "./components/Menu.vue";
 </script>
 
+
+<style>
+</style>
+
+
 <template>
 	<el-container>
 		<el-header>
-			<!-- 调用组件AppMenu，插入到el-header里面 -->
+			<!-- 调用组件AppMenu，作为菜单控件插入到el-header里面 -->
 			<AppMenu />
 		</el-header>
 		<el-main>
