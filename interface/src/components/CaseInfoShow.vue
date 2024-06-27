1d43ac1a61c252caecca8e9a7aa642f7eca50dec
UnconsciousChess <hacker_snowwolf@qq.com>
2024-06-28 00:18:34 +0800

    将案件信息展示的空间进一步区分，便于模块化管理；增加对案件的增删操作

--- interface/src/components/CaseInfoShow.vue
+++ interface/src/components/CaseInfoShow.vue
@@ -1,209 +1,109 @@
 <template>
-    	<!-- 下面是正式展示案件信息栏目 -->
-	<el-descriptions
-		class="margin-top"
-		:title="showText.title"
-		:column="2"
-		v-model="showText"
-		style="max-width: 700px"
-		border
-	>
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">案由</div>
+	<el-table :data="tableData" stripe style="width: 100%">
+		<el-table-column prop="index" width="40" type="expand">
+			<template #default>
+				<CaseInfoShowTable />
 			</template>
-			{{ showText.causeOfAction }}
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">标的额</div>
+		</el-table-column>
+		<el-table-column prop="index" label="序号" width="80" />
+		<el-table-column prop="causeOfAction" label="案由" width="250" />
+		<el-table-column prop="caseCourtCode" label="案号" width="250" />
+		<el-table-column fixed="right" label="操作" width="300">
+			<template #default="{row}">
+				<el-button type="primary" size="small">查看</el-button>
+				<el-button type="danger" size="small" @click="deleteData(row)"
+					>删除</el-button
+				>
+				<el-button type="success" size="small">输出</el-button>
+				<el-button type="info" size="small">上传</el-button>
 			</template>
-			{{ showText.litigationAmount }} 元
-		</el-descriptions-item>
+		</el-table-column>
+	</el-table>
 
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">管辖法院</div>
-			</template>
-			{{showText.courtName }}
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">法院案号</div>
-			</template>
-			{{ showText.caseCourtCode }}
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">委托阶段</div>
-			</template>
-			{{ showText.caseAgentStage }}
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">案件类型</div>
-			</template>
-			{{ showText.caseType }}
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">风险收费</div>
-			</template>
-			{{ showText.riskAgentStatus }}
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">调解意愿</div>
-			</template>
-			{{ showText.mediationIntention }}
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">前期风险收费金额</div>
-			</template>
-			{{ showText.riskAgentUpfrontFee }} 元
-		</el-descriptions-item>
-
-		<el-descriptions-item>
-			<template #label>
-				<div class="cell-item">后期风险收费比例</div>
-			</template>
-			{{ showText.riskAgentPostFeeRate }} %
-		</el-descriptions-item>
-	</el-descriptions>
-
-	<el-button 	@click="showText.showTextCreate()">
-		刷新案件信息
-	</el-button>
-
-	<div class="demo-collapse" style="max-width: 800px">
-		<el-collapse>
-			<el-collapse-item title="诉讼请求" name="1">
-				<el-text>
-					{{ showText.claimText }}
-				</el-text>
-			</el-collapse-item>
-			<el-collapse-item title="事实与理由" name="2">
-				<el-text>
-					{{ showText.factAndReason }}
-				</el-text>
-			</el-collapse-item>
-			<el-collapse-item title="拒绝调解理由" name="3">
-				<el-text>
-					{{ showText.rejectMediationReasonText }}
-				</el-text>
-			</el-collapse-item>
-		</el-collapse>
-	</div>
+	<el-button type="primary" @click="getTableData">刷新</el-button>
 </template>
 
 <script setup>
+import {ref, onMounted} from "vue";
 
-import { ref ,onMounted} from "vue";
+import CaseInfoShowTable from "./CaseInfoShowTable.vue";
 
+// 初始化tableData，为空数组，其内部格式为
+// {
+//     index: 1,
+//     causeOfAction: "案由",
+//     caseCourtCode: "案号",
+//     id : 时间戳
+// }
+const tableData = ref([]);
 
-const showText = ref({
-	title: "案件信息",
-	causeOfAction: "案由",
-	litigationAmount: "标的额",
-	courtName: "管辖法院",
-	caseCourtCode: "法院案号",
-	caseAgentStage: "委托阶段",
-	caseType: "案件类型",
-	riskAgentStatus: "风险收费",
-	mediationIntention: "调解意愿",
-	riskAgentUpfrontFee: "前期风险收费金额",
-	riskAgentPostFeeRate: "后期风险收费比例",
-	claimText: "诉讼请求",
-	factAndReason: "事实与理由",
-	rejectMediationReasonText: "拒绝调解理由",
-	plaintiffNameText: "原告姓名",
-	defendantNameText: "被告姓名",
+// 测试版本，模拟数据
+// const tableData = ref([
+//     {
+//         index: 1,
+//         causeOfAction: "案由1",
+//         caseCourtCode: "案号1",
+//         id: 1.1
+//     },
+//     {
+//         index: 2,
+//         causeOfAction: "案由2",
+//         caseCourtCode: "案号2",
+//         id: 2.3
+//     },
+//     {
+//         index: 3,
+//         causeOfAction: "案由3",
+//         caseCourtCode: "案号3",
+//         id: 3.4
+//     }
+// ]);
 
-	showTextCreate : function(){
-		// 从sessionStorage中获取案件信息
-		let caseFormModel = JSON.parse(sessionStorage.getItem("caseFormData"));
-		// 输出测试
-		console.log(caseFormModel);
-		// 将案件信息赋值给showText
-		this.caseCourtCode = caseFormModel.caseCourtCode;
-		this.causeOfAction = caseFormModel.causeOfAction;
-		this.litigationAmount = caseFormModel.litigationAmount;
-		this.courtName = caseFormModel.courtName;
-		this.claimText = caseFormModel.claimText;
-		this.factAndReason = caseFormModel.factAndReason;
-		this.rejectMediationReasonText = caseFormModel.rejectMediationReasonText;
-		this.riskAgentUpfrontFee = caseFormModel.riskAgentUpfrontFee;
-		this.riskAgentPostFeeRate = caseFormModel.riskAgentPostFeeRate;
-
-		// 下面是需要转换的字段
-		if (caseFormModel.riskAgentStatus == true) {
-			this.riskAgentStatus = "√";
-		} else {
-			this.riskAgentStatus = "X";
+// 从后端获取数据
+function getTableData() {
+    // 从前端获取数据
+	pywebview.api.OutputAllCaseInfoToFrontEnd().then((cases) => {
+		// 遍历cases，将数据根据一定的规则添加到tableData中
+		for (let i = 0; i < cases.length; i++) {
+            // 对比案件的id，如果相同则不添加
+            if (tableData.value.findIndex((item) => item.caseId === cases[i].caseId) !== -1) {
+                continue;
+            }
+			// 如果没有相同的，则将数据添加到tableData中
+			tableData.value.push({
+				index: i + 1,
+				causeOfAction: cases[i].causeOfAction,
+				caseCourtCode: cases[i].caseCourtCode, //temp test
+				caseId: cases[i].caseId,
+			});
 		}
 
-		if (caseFormModel.mediationIntention == true) {
-			this.mediationIntention = "√";
-		} else {
-			this.mediationIntention = "X";
-		}
-
-		if (caseFormModel.caseType == 1) {
-			this.caseType = "民事案件";
-		} else if (caseFormModel.caseType == 2) {
-			this.caseType = "行政案件";
-		} else {
-			this.caseType = "执行案件";
-		}
+        // 对tableData的index进行重新排序
+	    for (let i = 0; i < tableData.value.length; i++) {
+		tableData.value[i].index = i + 1;
+	    }
+	});
 
 
-		// 委托阶段转换
-		if (caseFormModel.caseAgentStage.length == 0) {
-			this.caseAgentStage = "无";
-		} else {
-			this.caseAgentStage = "";
-			// 遍历数组并转换为字符串
-			caseFormModel.caseAgentStage.forEach(stage =>{
-				if (stage == '1'){
-					this.caseAgentStage += "一审立案阶段" + "、";
-				}
-				else if (stage == '2'){
-					this.caseAgentStage += "一审诉讼阶段" + "、";
-				}
-				else if (stage == '3'){
-					this.caseAgentStage += "二审阶段" + "、";
-				}
-				else if (stage == '4'){
-					this.caseAgentStage += "执行阶段" + "、";
-				}
-				else if (stage == '5'){
-					this.caseAgentStage += "再审阶段" + "、";
-				}
-			})
-			// 如果最后一个字符是顿号，则去掉最后一个字符
-			if (this.caseAgentStage.charAt(this.caseAgentStage.length - 1) == "、"){
-				this.caseAgentStage = this.caseAgentStage.substring(0, this.caseAgentStage.length - 1);
-			}
-		}	
-	},
+}
 
+// 删除数据
+function deleteData(val) {
+	console.log("当前案件的id为" + val.caseId);
+	// 删除tableData中的数据
+	var deleteItemIndex = tableData.value.findIndex(
+		(item) => item.caseId === val.caseId
+	);
+	tableData.value.splice(deleteItemIndex, 1);
 
-});
+	// 对tableData的index进行重新排序
+	for (let i = 0; i < tableData.value.length; i++) {
+		tableData.value[i].index = i + 1;
+	}
+}
 
-
+// 加载页面时获取数据
 onMounted(() => {
-	// 在挂载组件的时候调用showTextCreate方法
-	showText.value.showTextCreate();
-	// console.log("案件信息展示组件挂载完毕");
+	getTableData();
 });
-
-
 </script>
