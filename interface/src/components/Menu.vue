2fa2ea713835ffb73b0d03a93d7b8bcb4790c681
UnconsciousChess <hacker_snowwolf@qq.com>
2024-06-30 10:01:11 +0800

    变更菜单顺序

--- interface/src/components/Menu.vue
+++ interface/src/components/Menu.vue
@@ -1,6 +1,6 @@
 <!-- 组合式Vue -->
 <script setup>
-import {ref, computed} from "vue";
+import { ref, computed } from "vue";
 // 导入router部分
 // import {useRoute,useRouter} from "vue-router";
 
@@ -12,19 +12,14 @@ const aboutDialog = ref({
 </script>
 
 <template>
-	<el-menu
-		:default-active="activeIndex"
-		:router="true"
-		class="el-menu-demo"
-		mode="horizontal"
-	>
+	<el-menu :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal">
 		<el-menu-item index="/">律师小只工具箱</el-menu-item>
 		<el-sub-menu index="2">
 			<template #title>文书生成器</template>
+			<el-menu-item index="/case-info-list">案件列表</el-menu-item>
 			<el-menu-item index="/case-info-form">案件信息录入</el-menu-item>
 			<el-menu-item disabled>模板文件选择</el-menu-item>
 			<el-menu-item disabled>归档材料生成</el-menu-item>
-			<el-menu-item index="/case-info-list">案件列表</el-menu-item>
 			<el-menu-item disabled>还没有想好的功能2</el-menu-item>
 		</el-sub-menu>
 		<el-sub-menu index="3">
@@ -40,12 +35,7 @@ const aboutDialog = ref({
 		<el-menu-item @click="aboutDialog.visible = true">关于</el-menu-item>
 	</el-menu>
 
-	<el-dialog
-		v-model="aboutDialog.visible"
-		title="关于本程序"
-		width="300"
-		draggable
-	>
+	<el-dialog v-model="aboutDialog.visible" title="关于本程序" width="300" draggable>
 		<p>Version:0.2</p>
 		<p>一只肥猫开发ing.....ZZzzzz</p>
 	</el-dialog>
