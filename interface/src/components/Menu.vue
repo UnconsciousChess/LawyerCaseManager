882ac11250f1ff140432484ef3aa1e85a5235a32
UnconsciousChess <hacker_snowwolf@qq.com>
2024-07-05 15:11:32 +0800

    加入测试路径

--- interface/src/components/Menu.vue
+++ interface/src/components/Menu.vue
@@ -20,7 +20,7 @@ const aboutDialog = ref({
 			<el-menu-item index="/case-info-form">案件信息录入</el-menu-item>
 			<el-menu-item index="/template-file">模板文件选择</el-menu-item>
 			<el-menu-item disabled>归档材料生成</el-menu-item>
-			<el-menu-item disabled>还没有想好的功能2</el-menu-item>
+			<el-menu-item index="/test">测试</el-menu-item>
 		</el-sub-menu>
 		<el-sub-menu index="3">
 			<template #title>其他工具</template>
