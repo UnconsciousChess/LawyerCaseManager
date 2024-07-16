8423e9768e11af25c5c88da2dc808298e6fd9fe4
UnconsciousChess <hacker_snowwolf@qq.com>
2024-07-16 23:11:25 +0800

    补全BankAccount的setter方法代码及对应的文档；修改input方法的方法名称

--- library/Property.py
+++ library/Property.py
@@ -1,4 +1,4 @@
-import sys
+import os,sys
 
 # 不要生成字节码
 sys.dont_write_bytecode = True
@@ -24,24 +24,41 @@ class BankAccount():
         return self._BankName
     
     # ======= Set方法 ======= #
+
     # 定义外部设置银行账户名的方法
     def SetAccountName(self,AccountName):
-        self._AccountName = AccountName
+        if isinstance(self._AccountName,str):
+            self._AccountName = AccountName
+            return
+        
     # 定义外部设置银行账户号码的方法
     def SetAccountNumber(self,AccountNumber):
-        self._AccountNumber = AccountNumber
+        if isinstance(self._AccountNumber,str):
+            if  AccountNumber.isdigit():
+                self._AccountNumber = AccountNumber
+                return
+            
     # 定义外部设置开户行的方法 
     def SetBankName(self,BankName):
-        self._BankName = BankName
-
+        if isinstance(self._BankName,str):
+            self._BankName = BankName
+            return
+        
     # ======= Input方法 ======= #
 
     # 直接通过一个txt用于输入银行账户信息
-    def InputBankAccountInfoFromTxt(self,InfoFile):
+    def InputInfoFromTxt(self,InfoFile) -> str:
+        # 先对传入的路径进行判断
+        if not os.path.exists(InfoFile):
+            return "PathNotExists"
+        if not os.path.isfile(InfoFile):
+            return "NotFile"
+        if InfoFile.endswith(".txt") == False:
+            return "NotTxtFile"
+        
         # 读取文件到列表中
         with open(file=InfoFile,mode="r",encoding="utf-8") as f:
             BankAccountInfoLines = f.readlines()
-            
         # 去除每行的换行符
         BankAccountInfoLines = [line.strip() for line in BankAccountInfoLines]
         for line in BankAccountInfoLines:
@@ -62,4 +79,4 @@ class BankAccount():
             # 开户行名称
             if key == "BankName":
                 self.SetBankName(information)
-        return
+        return "Success"
