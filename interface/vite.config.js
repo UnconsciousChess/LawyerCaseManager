118ebbbb4364b72635f71ff49ac0ffd25069b18d
UnconsciousChess <hacker_snowwolf@qq.com>
2024-09-02 09:27:54 +0800

    加入支持jsx

--- interface/vite.config.js
+++ interface/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
 import AutoImport from 'unplugin-auto-import/vite'
 
 import Components from 'unplugin-vue-components/vite'
@@ -15,6 +17,8 @@ export default defineConfig({
   plugins: [
 
     vue(),
+
+    vueJsx(),
 
     AutoImport({
 
