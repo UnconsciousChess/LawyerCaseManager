e0edb637ccc8cdb8316bfb1f3e0067a5bae79b02
UnconsciousChess <hacker_snowwolf@qq.com>
2024-09-12 10:45:37 +0800

    加入正确的路径和组件

--- interface/src/main.js
+++ interface/src/main.js
@@ -1,41 +1,40 @@
 // 导入App.vue
-import App from './App.vue'
+import App from "./App.vue";
 // 导入router
-import {createMemoryHistory, createRouter} from 'vue-router'
+import {createMemoryHistory, createRouter} from "vue-router";
 
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 // 导入组件
-import Caculator from './components/Caculator.vue'
-import Blank from './components/Blank.vue'
-// import CaseInfoShow from './components/CaseInfoShowTable.vue'
-import TemplateFileTable from './components/TemplateFileTable.vue'
+import Caculator from "./components/Caculator.vue";
+import Blank from "./components/Blank.vue";
+import CaseInfoShowNew from "./components/CaseInfoShowTable2.vue";
+import TemplateFileTable from "./components/TemplateFileTable.vue";
 
 // 设置路由
 const routes = [
-  {path: '/', component: Blank },
-  {path: '/caculator', component:Caculator},
-  {path: '/case-info-table', component: CaseInfoShowNew},
-  {path: '/template-file', component: TemplateFileTable},
-]
+	{path: "/", component: Blank},
+	{path: "/caculator", component: Caculator},
+	{path: "/case-info-table", component: CaseInfoShowNew},
+	{path: "/template-file", component: TemplateFileTable},
+];
 
 // 创建router的实例
 const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
-})
+	history: createMemoryHistory(),
+	routes,
+});
 
 // 创建Vue实例app
-const app = createApp(App)
+const app = createApp(App);
 
 // 注册ElementPlusIconsVue
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+	app.component(key, component);
 }
 
 // app使用router
-app.use(router)
+app.use(router);
 
 // 将app挂载到id为vueapp的dom上
-app.mount('#vueapp')
+app.mount("#vueapp");
